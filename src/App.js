e3258ee06da317637bc2f905c328f5fcfb4ca153
“ronaldssebalamu” <ronaldssebalamu2@gmail.com>
2023-02-15 06:05:13 +0300

    First-release

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Book from "./Components/Book";
 
 function App() {
+
+  // const books = () => data.map(book)=>{
+  //   return(
+  //     <div></div>
+  //   )
+  // }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Journal Today</h2>
+      <Book />
     </div>
   );
 }
